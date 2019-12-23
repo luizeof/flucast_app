f5046ef7ca246f4a397fef9b8e3d5435d21e997e
Luiz Eduardo <luizeof@gmail.com>
2019-12-23 15:44:48 -0300

    Classes

---
+++ lib/global.dart
@@ -0,0 +1,7 @@
+import 'package:dart_pod/dart_pod.dart';
+
+String feed_url = "https://anchor.fm/s/848f2e4/podcast/rss";
+
+Podcast myPodcast;
+
+List<Episode> episodes = <Episode>[];
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flucast_app/main.dart';
+import 'package:flucast_app/home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(FluCastApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
