427f427d0a5486de0c1b5bbba9dc8e6afd7c71d8
Luiz Eduardo <luizeof@gmail.com>
2019-12-26 06:23:25 -0300

    Update feed.dart

--- lib/feed.dart
+++ lib/feed.dart
@@ -1,3 +1,3 @@
 /// The Podcast RSS Feed
 final podcastFeedUrl =
-    "https://feeds.soundcloud.com/users/soundcloud:users:3359843144/sounds.rss";
+    "https://feeds.soundcloud.com/users/soundcloud:users:335943144/sounds.rss";
