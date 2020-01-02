dda68ccc5fe2aeb77d359ee65610163d8920b6aa
Luiz Eduardo <luizeof@gmail.com>
2020-01-02 08:54:49 -0300

    SEMVER

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+major=$(cat version/MAJOR)
+minor=$(cat version/MINOR)
+patch=$(expr $(cat version/PATCH) + 1)
+echo $patch >version/PATCH
+build=$(git rev-list HEAD --count)
+
+echo "major: $major"
+echo "major: $minor"
+echo "major: $patch"
+echo "build: $build"
+
 flutter build appbundle --target-platform android-arm,android-arm64,android-x64
