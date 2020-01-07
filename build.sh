e9cc6a55f60e254f958c0eeae223bc76007410de
Luiz Eduardo <luizeof@gmail.com>
2020-01-07 08:42:02 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
-major=$(cat version/MAJOR)
+if [ -f version/MAJOR ]; then
+    major=$(cat version/MAJOR)
+else
+    major=0
+fi
+
+if [ -f version/MINOR ]; then
+    minor=$(cat version/MINOR)
+else
+    minor=0
+fi
+
+if [ -f version/PATCH ]; then
+    patch=$(cat version/PATCH)
+else
+    patch=0
+fi
+
+if [ -f version/BUILD ]; then
+    build=$(cat version/BUILD)
+else
+    build=0
+fi
+
 minor=$(cat version/MINOR)
 patch=$(expr $(cat version/PATCH) + 1)
 echo $patch >version/PATCH
+echo $major >version/MAJOR
+echo $minor >version/MINOR
 build=$(git rev-list HEAD --count)
+echo $build >version/BUILD
 
 echo "major: $major"
 echo "major: $minor"
